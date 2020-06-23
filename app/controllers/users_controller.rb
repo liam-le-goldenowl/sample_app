18204d1f2ae8e6a0f6ff67cc4dc2b6e5d2fd781a
liam-le-goldenowl <liam.le.goldenowl@gmail.com>
2020-06-24 02:41:49 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,26 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    if @user.save
+      flash[:success] = 'Welcome to blog'
+      redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
